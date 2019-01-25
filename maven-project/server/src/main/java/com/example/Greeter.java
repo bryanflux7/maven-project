fb79720366fd17bcd6ffe9c6414077ed7f049b52
unknown <bryan@flux7.com>
2019-01-25 16:04:29 -0600

    fix stuff

--- maven-project/server/src/main/java/com/example/Greeter.java
+++ maven-project/server/src/main/java/com/example/Greeter.java
@@ -16,11 +16,9 @@ public class Greeter {
 	*this is stuff
 	*/
 
-
 	public Greeter(){
 
 	}
-
 
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
