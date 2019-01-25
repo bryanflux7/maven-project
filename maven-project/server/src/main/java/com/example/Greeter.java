aab71858fe7beda2783df5c9e4f0c58809348e27
unknown <bryan@flux7.com>
2019-01-25 15:36:30 -0600

    fix stuff

---
+++ maven-project/server/src/main/java/com/example/Greeter.java
@@ -0,0 +1,27 @@
+package com.example;
+
+/**
+ * This is a class.
+ */
+public class Greeter {
+
+  /**
+   * This is a constructor.
+   */
+  public Greeter() {
+
+  }
+
+	/**
+	*this is stuff
+	*/
+
+
+	public Greeter(){
+
+	}
+
+  public final String greet(final String someone) {
+    return String.format("Hello, %s!", someone);
+  }
+}
