298121a6030fe7e5ab90ed6429484a3dff3101d0
unknown <bryan@flux7.com>
2019-01-25 16:23:08 -0600

    fix stuff

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -1,17 +1,16 @@
- package com.example;
+package com.example;
 
  /**
   * This is a class.
   */
- public class Greeter { 
-   /**
-    * This is a constructor.
-    */
+  public class Greeter { 
+    /**
+     * This is a constructor.
+     */
    public Greeter() {
-
+ 
+   }
+    public String greet(String someone) {
+     return String.format("Hello, %s!", someone);
+   }
   }
-
-   public String greet(String Final someone) {
-    return String.format("Hello, %s!", someone);
-  }
- }
